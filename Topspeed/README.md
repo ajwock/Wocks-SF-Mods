72c99007a51d8db0bfa6dfc3200b4cbde7396b42
Andrew Wock <ajwock@gmail.com>
2020-04-03 14:51:28 -0400

    Adding topspeed mod

---
+++ Topspeed/README.md
@@ -0,0 +1,5 @@
+# Jetpack
+
+Gives players the ability to accelerate while flying in any direction.
+
+**Note:** This mod disables high score reporting.
---
+++ Topspeed/Topspeed.cs
@@ -0,0 +1,33 @@
+﻿using SFMF;
+using System;
+using System.IO;
+using UnityEngine;
+
+namespace Topspeed
+{
+    public class Topspeed : IMod
+    {
+        private const string SettingsPath = @".\SFMF\ModSettings\Topspeed.csv";
+
+        private float newTopSpeed { get; set; }
+
+        private void Start()
+        {
+            var settings = File.ReadAllLines(SettingsPath);
+
+            foreach (var line in settings)
+            {
+                if (line == "")
+                    continue;
+
+                var parts = line.Split(',');
+
+                if (parts[0] == "Setting")
+                    newTopSpeed = float.Parse(parts[2]);
+            }
+
+            PlayerMovement.Singleton.forwardSpeedLimits.max = newTopSpeed;
+        }
+
+    }
+}
